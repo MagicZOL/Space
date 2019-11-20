3e851186bd9c0b5980011b511dae0c088aaca531
MagicZL0 <tnwls9721@naver.com>
2019-11-20 17:28:10 +0900

    달 공전함

---
+++ Assets/Moon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moon : MonoBehaviour
+{
+    public GameObject Earth;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Earth.transform.position, Vector3.down, speed * Time.deltaTime);
+    }
+}
